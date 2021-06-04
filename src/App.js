cbe62db271562a8d64e56456a95e2fdec86d2dc5
David Folkins <davidf@EXS-Programmer11.local>
2021-06-04 16:02:03 -0400

    Add inital layout for desktop view

--- src/App.js
+++ src/App.js
@@ -9,11 +9,26 @@ import ManyIceCream from "./components/ManyIceCream"
 const useStyles = makeStyles(() => ({
   container: {
     display: "flex",
-    flexFlow: "row wrap",
-    backgroundColor: "#ffebc7"
+    flexDirection: "row",
+    backgroundColor: "#ffebc7",
+    alignItems: "center",
+      justifyContent: "center",
+    margin: "60px 15%"
+  },
+  col2: {
+    display: "flex",
+    flexDirection: "column",
+    flex: "5"
+  },
+  twoicecreams: {
+    display: "flex",
+    flexDirection: "row",
   },
   image4lg: {
-    display: "block"
+    display: "block",
+    flex: "1",
+    marginTop: "56px",
+    marginLeft: "80px"
   },
   image4sm: {
     display: "none"
@@ -24,12 +39,27 @@ const useStyles = makeStyles(() => ({
   image1sm: {
     display: "none"
   },
+  image2: {
+    marginLeft: "80px"
+  },
+  image3: {
+    marginLeft: "40px"
+  },
   '@media screen and (max-width: 1200px)': {
     container: {
       display: "flex",
       flexDirection: "column",
       alignItems: "center",
-      justifyContent: "center"
+      justifyContent: "center",
+      margin: "0px"
+    },
+    col2: {
+      display: "flex",
+      flexDirection: "column",
+    },
+    twoicecreams: {
+      display: "flex",
+      flexDirection: "column",
     },
     image4lg: {
       display: "none"
@@ -44,8 +74,13 @@ const useStyles = makeStyles(() => ({
       display: "block",
       marginBottom: "20px"
     },
-    image: {
-      marginBottom: "20px"
+    image2: {
+      marginBottom: "20px",
+      marginLeft: "0px"
+    },
+    image3: {
+      marginBottom: "20px",
+      marginLeft: "0px"
     }
   }
 }));
@@ -61,17 +96,21 @@ function App() {
       <div className={classes.image1sm}>
         <ManyIceCream />
       </div>
-      <div className={classes.image}>
-        <GreenIceCream />
-      </div>
-      <div className={classes.image}>
-        <PurpleIceCream />
-      </div>
-      <div className={classes.image4sm}>
-        <FourCones />
-      </div>
-      <div className={classes.image4lg}>
-        <Philosophy />
+      <div className={classes.col2}>
+        <div className={classes.twoicecreams}>
+          <div className={classes.image2}>
+            <GreenIceCream />
+          </div>
+          <div className={classes.image3}>
+            <PurpleIceCream />
+          </div>
+        </div>
+        <div className={classes.image4sm}>
+          <FourCones />
+        </div>
+        <div className={classes.image4lg}>
+          <Philosophy />
+        </div>
       </div>
     </div>
   );
