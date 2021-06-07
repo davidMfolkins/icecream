282b343a63b20690ba205aabfda9f7d6117f08fd
David Folkins <davidf@EXS-Programmer11.local>
2021-06-07 10:30:37 -0400

    add text to many icecream image

--- src/components/ManyIceCream.js
+++ src/components/ManyIceCream.js
@@ -2,6 +2,20 @@ import manyicecream from "../images/manyicecream.png"
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
+  text: {
+    color: "white",
+    zIndex: "100",
+    position: "absolute",
+    top: "32%",
+    height: "140px",
+    backdropFilter: "blur(2px)",
+    textAlign: "center",
+    marginLeft: "2px",
+    width: "414px"
+  },
+  h1: {
+    textShadow: "3px 3px 5px #000000"
+  },
   img: {
     border: "1px solid #636363"
   }
@@ -12,6 +26,10 @@ function ManyIceCream() {
   
   return (
     <div>
+      <div className={classes.text}>
+        <h1 className={classes.h1}>Top 10 Hottest Flavours</h1>
+        <h1 className={classes.h1}>Summer 2021</h1>
+      </div>
       <img src={manyicecream} className={classes.img} width="415px" alt="many-ice-cream" />
     </div>
   );
