8175d5b5168a45469cdabf70ff38e5bb3dfa0735
David Folkins <davidf@EXS-Programmer11.local>
2021-06-23 12:49:28 -0400

    fix margins

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const useStyles = makeStyles(() => ({
     alignItems: "center",
     justifyContent: "center",
     marginTop: "5%",
+    marginBottom: "5%",
     marginLeft: "15%",
     marginRight: "15%"
   },
